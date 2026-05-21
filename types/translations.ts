3b6b7861980a7e58767f4a7ea5a9e7c43b27b8d4
Daniel-Fonseca-da-Silva <daniel117fonseca@gmail.com>
2026-05-21 21:36:04 +0100

    feat: added the file about translations

---
+++ types/translations.ts
@@ -0,0 +1,40 @@
+export interface BenefitItem {
+  icon: string;
+  title: string;
+  description: string;
+}
+
+export interface SocialProofStat {
+  number: string;
+  label: string;
+  color: string;
+}
+
+export interface DemoFeature {
+  title: string;
+  description: string;
+}
+
+export interface DemoStat {
+  number: string;
+  label: string;
+  color: string;
+}
+
+export interface ResultsMetric {
+  number: string;
+  label: string;
+}
+
+export interface TestimonialItem {
+  quote: string;
+  authorName: string;
+  authorRole: string;
+  resultValue: string;
+  resultLabel: string;
+}
+
+export interface FaqItem {
+  question: string;
+  answer: string;
+}
